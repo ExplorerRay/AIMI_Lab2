ca76de678cac4fe088797f0e7e8e61abb90d3b08
Ray Huang <47706097+ExplorerRay@users.noreply.github.com>
2023-04-08 11:48:15 +0800

    Add files via upload
    first upload

---
+++ models/EEGNet.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+
+# TODO implement EEGNet model
+class EEGNet(nn.Module):
+    def __init__(self):
+        super(EEGNet, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
